2522e4a71a6c92df5198acc541bafbd87e8cf2ef
devanshASU <devansh.gupta73@yahoo.in>
2019-08-28 00:06:16 +0530

    Using Port From ENV
    Signed-off-by: devanshASU <devansh.gupta73@yahoo.in>

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const path=require('path')
 
+const SERVER_PORT = process.env.PORT || 3333
+
 const app=express()
 app.use('/public',express.static(path.join(__dirname,'public')))
 
 app.get('/',(req,res)=> res.send("HELLO FROM THE BACKEND"))
 
-app.listen(3333,()=> console.log("SERVER STARTED AT http://localhost:3333"))
+app.listen(SERVER_PORT,()=> console.log("SERVER STARTED AT http://localhost:3333"))
